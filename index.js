c6fa63a986d1e1596097a27666336d8f2f8f43e4
barunkaa <32076141+barunkaa@users.noreply.github.com>
2017-11-11 20:24:27 +0000

    added background picture

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 window.onload = function() {
+	var image = document.getElementById('image');
+	var i = 0;
 	
+	setTimeout(function() {
+		var src = './img/frontpage' + i + '.jpg';
+		var url = "url('" . src . "')";
+		image.style.backgroundImage = url;
+		console.log(i);
+		i++;
+	}, 5000);
 };
