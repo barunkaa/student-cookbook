34214cba2a944dcc1a3a79e830d600847fed3383
barunkaa <32076141+barunkaa@users.noreply.github.com>
2017-11-12 16:26:13 +0000

    added pages with recipes and recipe menu

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+var images = [];
+var index = 0;
+
+images[0] = "img/frontpage1.jpg";
+images[1] = "img/frontpage2.jpg";
+images[2] = "img/frontpage3.jpg";
+
 window.onload = function() {
-	var image = document.getElementById('image');
-	var i = 0;
-	
-	setTimeout(function() {
-		var src = './img/frontpage' + i + '.jpg';
-		var url = "url('" . src . "')";
-		image.style.backgroundImage = url;
-		console.log(i);
-		i++;
-	}, 5000);
+	changeImage();
 };
+
+function changeImage() {
+	var img = document.getElementById("image");
+	img.src = images[index];
+	index++;
+
+	if (index >= images.length){
+		index = 0;
+	}
+	setTimeout("changeImage()", 3000);
+}
